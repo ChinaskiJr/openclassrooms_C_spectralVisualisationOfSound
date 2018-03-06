641d5e6130fd7bcf7745e076ff3e784ee000b07c
ChinaskiJr <robin.colombier@gmail.com>
2018-03-06 18:13:34 +0100

    Initialisation of SDL, window, renderer

--- main.c
+++ main.c
@@ -4,8 +4,56 @@
 #include <FMOD/fmod.h>
 #include <FMOD/fmod_errors.h>
 
+#define WINDOW_WEIGHT   800
+#define WINDOW_HEIGHT   600
+
 int main(int argc, char *argv[]) {
 
-    return 0;
+    int exitStatus = EXIT_FAILURE;
+
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_Rect screen = {0, 0, WINDOW_WEIGHT, WINDOW_HEIGHT};
+
+    if (0 != SDL_Init(SDL_INIT_TIMER | SDL_INIT_VIDEO | SDL_INIT_EVENTS)) {
+        fprintf(stderr, "SDL_Init Error : %s\n", SDL_GetError());
+        goto QuitSDL;
+    }
+
+    window = SDL_CreateWindow("Spectral Visualisation of Chinaski", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WEIGHT, WINDOW_HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+    if (window == NULL) {
+        fprintf(stderr, "SDL_CreateWindow Error : %s\n", SDL_GetError());
+        goto QuitSDL;
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL) {
+        fprintf(stderr, "SDL_CreateRenderer Error : %s\n", SDL_GetError());
+        goto QuitSDL;
+    }
+
+    if (0 != SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255)) {
+        fprintf(stderr, "SDL_SetRenderDrawColor Error : %s\n", SDL_GetError());
+        goto QuitSDL;
+    }
+    
+    if (0 != SDL_RenderClear(renderer)) {
+        fprintf(stderr, "SDL_RenderClear Error : %s\n", SDL_GetError());
+        goto QuitSDL;
+    }
+
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(4000);
+    
+    exitStatus = EXIT_SUCCESS;
+
+QuitSDL:
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    fprintf(stderr, "%d\n", exitStatus);
+    return exitStatus;
 }
 
