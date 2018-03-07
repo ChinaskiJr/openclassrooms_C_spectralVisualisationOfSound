e92990d1c44cfd083bf4463e654e64b4b93adbc6
ChinaskiJr <robin.colombier@gmail.com>
2018-03-07 17:01:08 +0100

    Open a window and play the sound if you press space key

--- main.c
+++ main.c
@@ -24,6 +24,8 @@ int main(int argc, char *argv[]) {
     SDL_Window *window = NULL;
     SDL_Renderer *renderer = NULL;
     SDL_Rect screen = {0, 0, WINDOW_WEIGHT, WINDOW_HEIGHT};
+    SDL_Event event;
+    SDL_bool quit = SDL_FALSE;
 
     FMOD_RESULT result = 0;
     FMOD_SYSTEM *system = NULL;
@@ -36,7 +38,7 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "SDL_Init Error : %s\n", SDL_GetError());
         goto QuitSDL;
     }
-    
+
     window = SDL_CreateWindow("Spectral Visualisation of Chinaski", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WEIGHT, WINDOW_HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
     if (window == NULL) {
         fprintf(stderr, "SDL_CreateWindow Error : %s\n", SDL_GetError());
@@ -53,7 +55,7 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "SDL_SetRenderDrawColor Error : %s\n", SDL_GetError());
         goto QuitSDL;
     }
-    
+
     if (0 != SDL_RenderClear(renderer)) {
         fprintf(stderr, "SDL_RenderClear Error : %s\n", SDL_GetError());
         goto QuitSDL;
@@ -82,27 +84,45 @@ int main(int argc, char *argv[]) {
         goto QuitFMOD;
     }
 
-    // Load, play and send the mp3 to the channel
-    result = FMOD_System_CreateSound(system, "LeavingEarth.mp3", FMOD_CREATESTREAM, NULL, &music);
-    if (result != FMOD_OK) {
-        fprintf(stderr, "FMOD_System_CreateSound Error : %s\n", FMOD_ErrorString(result));
-        goto QuitFMOD;
-    }
-    
-    result = FMOD_System_PlaySound(system, music, NULL, 0, &channel0);
-    if (result != FMOD_OK) {
-        fprintf(stderr, "FMOD_System_PlaySound Error : %s\n", FMOD_ErrorString(result));
-        goto QuitFMOD;
+
+    while(!quit) {
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    quit = SDL_TRUE;
+                    break;
+                case SDL_KEYDOWN:
+                    switch(event.key.keysym.sym) {
+                        case SDLK_ESCAPE:
+                            quit = SDL_TRUE;
+                            break;
+                        case SDLK_SPACE:
+                            // Load, play and send the mp3 to the channel
+                            result = FMOD_System_CreateSound(system, "LeavingEarth.mp3", FMOD_CREATESTREAM, NULL, &music);
+                            if (result != FMOD_OK) {
+                                fprintf(stderr, "FMOD_System_CreateSound Error : %s\n", FMOD_ErrorString(result));
+                                goto QuitFMOD;
+                            }
+
+                            result = FMOD_System_PlaySound(system, music, NULL, 0, &channel0);
+                            if (result != FMOD_OK) {
+                                fprintf(stderr, "FMOD_System_PlaySound Error : %s\n", FMOD_ErrorString(result));
+                                goto QuitFMOD;
+                            }
+
+                            result = FMOD_Channel_IsPlaying(channel0, isPlaying);
+                            if (result != FMOD_OK) {
+                                fprintf(stderr, "FMOD_Channel_IsPlaying Error : %s\n", FMOD_ErrorString(result));
+                                goto QuitFMOD;
+                            }
+                            break;
+                    }
+
+
+            }
+        }
     }
 
-    result = FMOD_Channel_IsPlaying(channel0, isPlaying);
-    if (result != FMOD_OK) {
-        fprintf(stderr, "FMOD_Channel_IsPlaying Error : %s\n", FMOD_ErrorString(result));
-        goto QuitFMOD;
-    }
-
-    SDL_Delay(400000);
-    
     exitStatus = EXIT_SUCCESS;
 
 QuitFMOD:
